873e924d795d4ae8dcec11f015f770cc0434c616
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-10-05 17:03:44 +0200

    change back

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -102,7 +102,7 @@ export default function Navbar() {
                       className="relative h-12 w-12 rounded-full border-2 border-[#8b4513] hover:bg-[#c4b17e] transition-colors duration-200"
                     >
                       <Avatar className="h-10 w-10">
-                        <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+                        <AvatarImage className="bg-yellow-100" src="/avatars/feather.png" alt="@shadcn" />
                         <AvatarFallback>SC</AvatarFallback>
                       </Avatar>
                     </Button>
