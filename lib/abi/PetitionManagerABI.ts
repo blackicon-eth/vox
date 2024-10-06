1047ce2abe8ca548d02b3d40bfa2b9d41ddbeddd
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-10-06 03:09:14 +0200

    Update fhenixjs version to 0.3.0

---
+++ lib/abi/PetitionManagerABI.ts
@@ -0,0 +1,245 @@
+export const PetitionManagerContract =
+  "0xe20E81DeD401367F694D9E90628C8d1BDca1D87a";
+
+export const PetitionManagerABI = [
+  {
+    inputs: [],
+    name: "AlreadyVoted",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_creator",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "_title",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_description",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_extendedDescription",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_endDate",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_goal",
+        type: "uint256",
+      },
+    ],
+    name: "createPetition",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "PetitionDoesNotExist",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "PetitionExpired",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "petitionId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "title",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "goal",
+        type: "uint256",
+      },
+    ],
+    name: "PetitionCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_petitionId",
+        type: "uint256",
+      },
+    ],
+    name: "vote",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "petitionId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "voter",
+        type: "address",
+      },
+    ],
+    name: "Voted",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_petitionId",
+        type: "uint256",
+      },
+    ],
+    name: "getPetition",
+    outputs: [
+      {
+        internalType: "string",
+        name: "title",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "extendedDescription",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "endDate",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "goal",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "votes",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "hasVoted",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "petitionCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "petitions",
+    outputs: [
+      {
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "title",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "extendedDescription",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "endDate",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "goal",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "votes",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
